c03c84c0dd85a2f82fe6c22fd9318c1c1a722f1f
Amaru Cuba Gyllensten <amarucuba@gmail.com>
2024-02-08 20:48:39 +0100

    fix

--- books/litbank/process/process.sh
+++ books/litbank/process/process.sh
@@ -2,7 +2,7 @@
 
 OUT=/output/litbank.json
 
-ls input/*.epub > tmp-file
+ls /input/*.epub > tmp-file
 
 parallel -a tmp-file pandoc --from epub --to plain | jq -Rsc 'capture("(?<prefix>^.+?Litteraturbanken.se – The Swedish Literature Bank\\s++)(?<text>.*+$\\S)\\s*$"; "m")' > $OUT
 
